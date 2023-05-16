a5938b82964dff787e9afa731c7a86619d0c234b
unknown <Raj Dama>
2023-05-16 10:23:28 +0530

    Docker Compose config added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/",(req,res)=>{
-    res.send("<h1>Hello Raj<h1>")
+    res.send("<h1>Hello Raj Dama<h1>")
 })
 
 app.listen(3000,()=>{console.log("listening......")})
