50882a7e38309bd32a2fb3be288c3088aef76c95
unknown <Raj Dama>
2023-05-17 17:37:37 +0530

    mongodb image created and integrated with mongoose

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express")
+const mongoose = require("mongoose")
+
 const app = express()
+mongoose.connect(
+    "mongodb://raj:1234@mongo:27017/?authSource=admin"
+).then(()=>{console.log("successfully connected")}).catch((e)=>{console.log(e)})
 
 app.get("/",(req,res)=>{
     res.send("<h1>Hello Raj Dama yoo !!<h1>")
